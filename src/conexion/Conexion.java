413cb61c218a120bc72d43a0a72499e9f929cfde
omarsmarinhos <omarmarinosaguilar@gmail.com>
2022-07-13 05:03:01 -0500

    Prueba

--- src/conexion/Conexion.java
+++ src/conexion/Conexion.java
@@ -15,9 +15,14 @@ public class Conexion {
         Connection connection = null;
 
         try {
+//            Class.forName("com.mysql.jdbc.Driver");
+//            connection = DriverManager.getConnection(
+//                    "jdbc:mysql://7qtgzkyctnod.us-east-3.psdb.cloud/poo-lianfarma?useSSL=true&sslMode=VERIFY_IDENTITY",
+//                    "jyw8llfvew1w",
+//                    "pscale_pw_k3x3W3R3oYK03yZd4-m5iSXeHIWV1plcTmu-6gWhVls");
             Class.forName("com.mysql.jdbc.Driver");
             connection = DriverManager.getConnection(URL + DB + "?useSSL=false", USER, PASSWORD);
-            //System.out.println("conexion ok");
+//            System.out.println("conexion ok");
         } catch ( SQLException | ClassNotFoundException e) {
             System.out.println(e.toString());
         } finally {
